d548aba26dff5d80bf7246725f4d9fd0e894c713
sharifboy <sharifboy@gmail.com>
2023-03-19 11:44:08 +0500

    added string

---
+++ app.js
@@ -0,0 +1,27 @@
+// STRING EXAMPLE
+console.log('Hello world')
+
+let email = 'sharifboy@gmail.com'
+console.log(email);
+
+// STRING CONCATENATION Birlashtirish\
+let firstName = 'sharifboy'
+let lastName = 'olloyorov'
+
+let fullName = firstName+ ' ' +lastName
+console.log(fullName);
+
+// GETTING CHARACTERS 
+console.log(fullName[2]);
+
+// STRING LENGTH => Uzunlik
+console.log(fullName.length);
+
+// STRING METHODS Funktsiyaga o'xshash ish bajradi 
+let result = fullName.toLocaleLowerCase()
+console.log(result);
+
+console.log(fullName.toUpperCase());
+
+let index = email.indexOf('y')
+console.log(index);
